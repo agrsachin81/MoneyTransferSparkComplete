70bad4d3d66b3f924ea014732764082c6b9599ec
SachinAgrawal <agrsachin81@gmail.com>
2019-10-02 21:28:35 +0530

    Added comments test level why test were written

--- src/test/java/home/test/api/example/moneytransfer/spi/utils/TransactionRekuestImplTest.java
+++ src/test/java/home/test/api/example/moneytransfer/spi/utils/TransactionRekuestImplTest.java
@@ -6,9 +6,6 @@ import static org.junit.Assert.assertEquals;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -17,25 +14,21 @@ import com.google.gson.Gson;
 import home.test.api.example.moneytransfer.spi.interfaces.TransactionRekuest;
 import home.test.api.example.moneytransfer.util.GsonHelper;
 
+/**
+ * Just to test whether after each deserialization the object must never return null 
+ * this should even work after changing JSON deserializer/deserializer
+ * Dev must use the same JSON deserializer object as used by the restful api router 
+ * @author sachin
+ *
+ */
 public class TransactionRekuestImplTest {
 
-	static Gson json = GsonHelper.createJsonSerializerDeserializer();
+	private static Gson json=null;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		json = GsonHelper.createJsonSerializerDeserializer();
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-	
 	@Test
 	public void testJsonSerializationNotNullTransfer() {
 
